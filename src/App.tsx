89c4024090e6f431407cb3b5e8b5c7d8cce4f6cf
BadalGupta7 <128031071+BadalGupta7@users.noreply.github.com>
2024-11-09 23:30:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ListTodo } from "./Components/ListTodo";
 import { Task } from "./types";
 
 const App: React.FC = () => {
-  
+  // hI
   const [todoList, setTodoList] = useState<Task[]>(
     JSON.parse(localStorage.getItem("todosList") || "[]")
   );
